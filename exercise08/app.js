038dd99a6dc2c63b8f00cfd97393c5a89c8b1d42
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-03 13:45:21 -0700

    Added

---
+++ exercise08/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let personNaem = "Shah Meer";
+console.log(`My Name is ${personNaem}`);
+let now = new Date();
+console.log(now);
---
+++ exercise08/app.ts
@@ -0,0 +1,5 @@
+let personNaem : string = "Shah Meer" ;
+console.log(`My Name is ${personNaem}`);
+
+let now = new Date();
+console.log(now);
