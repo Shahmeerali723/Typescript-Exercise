828cdea0b48d13e93ff2c5142275393b7af6a28d
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-03 11:28:17 -0700

    Added

---
+++ exercise02/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "Shah Meer";
+console.log(personName.toUpperCase()); //Upper-case
+console.log(personName.toLowerCase()); //Lower-Case
+console.log(personName[0].toUpperCase() + personName.slice(1).toLowerCase()); //Title-Case
---
+++ exercise02/readme.md
@@ -0,0 +1,6 @@
+## 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+Initialize Typescript in your Project.
+
+        tsc --init
+
