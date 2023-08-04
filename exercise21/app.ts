138e89ac6ddfc928f7832515ef46337228f6e014
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-04 10:30:00 -0700

    Added commit

---
+++ exercise21/app.ts
@@ -0,0 +1,45 @@
+let place : string[] = [
+    "Silicon Valley",
+    "Germany",
+    "Isral",
+    "Egypt",
+    "Battle-Ground"
+]
+
+console.log(place); //print array
+
+console.log(place.sort());  //sort Alphabates level.
+
+console.log(place); // original order
+
+console.log(place.sort().reverse()); // reverse order
+
+console.log(place); // original order
+
+place.reverse(); // reverse order
+console.log(place);
+
+place.reverse(); // same again
+console.log(place);
+
+place.sort(); // sort 
+console.log(place);
+
+place.sort().reverse(); // sort + recerse
+console.log(place);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
