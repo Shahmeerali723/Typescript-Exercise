4b1ab667bf1bd7b503b5edb430e9241282143dd3
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-04 17:42:41 -0700

    Add Exercise 25

--- exercise25/app.ts
+++ exercise25/app.ts
@@ -1,21 +1,58 @@
-// Define a variable to store the alien's color
+// Define a variable to store the alien’s color 
 let alien_color: string = "green";
 
-// Write an if statement to test whether the alien's color is green
-if (alien_color == "green") {
-  // Print a message that the player just earned 5 points
-  console.log("You just earned 5 points!");
-}
+// Write an if-elif-else chain to test the alien’s color 
+if (alien_color == "green") { // Print a message that the player earned 5 points 
+  console.log("You earned 5 points!"); } 
+  else if (alien_color == "yellow") { // Print a message that the player earned 10 points 
+    console.log("You earned 10 points!"); } 
+    else if (alien_color == "red") { // Print a message that the player earned 15 points 
+      console.log("You earned 15 points!"); } 
+      else { // Print a message that the alien’s color is unknown 
+        console.log("The alien’s color is unknown."); }
 
-// Output: You just earned 5 points!
+// Output: You earned 5 points!
 
-// Define a variable to store the alien's color
-let alien_color1: string = "red";
+// Define a variable to store the alien’s color 
+let alien_color1: string = "yellow";
 
-// Write an if statement to test whether the alien's color is green
-if (alien_color1 == "green") {
-  // Print a message that the player just earned 5 points
-  console.log("You just earned 5 points!");
-}
+// Write an if-elif-else chain to test the alien’s color 
+if (alien_color1 == "green") { // Print a message that the player earned 5 points 
+  console.log("You earned 5 points!"); } 
+  else if (alien_color1 == "yellow") { // Print a message that the player earned 10 points 
+    console.log("You earned 10 points!"); } 
+    else if (alien_color1 == "red") { // Print a message that the player earned 15 points 
+      console.log("You earned 15 points!"); } 
+      else { // Print a message that the alien’s color is unknown 
+        console.log("The alien’s color is unknown."); }
 
-// Output: (nothing)
+// Output: You earned 10 points!
+
+let alien_color2 : string = "red";
+
+// Write an if-elif-else chain to test the alien’s color 
+if (alien_color2 == "green") { // Print a message that the player earned 5 points 
+  console.log("You earned 5 points!"); } 
+  else if (alien_color2 == "yellow") { // Print a message that the player earned 10 points 
+    console.log("You earned 10 points!"); } 
+    else if (alien_color2 == "red") { // Print a message that the player earned 15 points 
+      console.log("You earned 15 points!"); } 
+      else { // Print a message that the alien’s color is unknown 
+        console.log("The alien’s color is unknown."); }
+
+// Output: You earned 15 points!
+
+let alien_color3 : string = "blue";
+
+// Write an if-elif-else chain to test the alien’s color 
+if (alien_color3 == "green") { // Print a message that the player earned 5 points 
+  console.log("You earned 5 points!"); } 
+  else if (alien_color3 == "yellow") { // Print a message that the player earned 10 points 
+    console.log("You earned 10 points!"); } 
+    else if (alien_color3 == "red") { // Print a message that the player earned 15 points 
+      console.log("You earned 15 points!"); } 
+      else { // Print a message that the alien’s color is unknown 
+        console.log("The alien’s color is unknown."); }
+
+// Output: The alien’s color is unknown.
+
