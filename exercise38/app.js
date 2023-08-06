c2a70176252606e6d016dee86d8f0009e323ea1f
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-06 13:59:16 -0700

    Add Commit

--- exercise38/app.js
+++ exercise38/app.js
@@ -1,17 +1,18 @@
 "use strict";
-// Define a variable to store the alien's color
-let alien_color = "green";
-// Write an if statement to test whether the alien's color is green
-if (alien_color == "green") {
-    // Print a message that the player just earned 5 points
-    console.log("You just earned 5 points!");
+// Write a function called make_album() that builds a Object // describing a music album 
+function make_album(artist, title, tracks) {
+    let album = { artist: artist, title: title };
+    // Add an optional parameter to make_album() that allows you to store the // number of tracks on an album. If the calling line includes a value for the number // of tracks, add that value to the album’s Object 
+    if (tracks) {
+        album.tracks = tracks;
+    }
+    return album;
 }
-// Output: You just earned 5 points!
-// Define a variable to store the alien's color
-let alien_color1 = "red";
-// Write an if statement to test whether the alien's color is green
-if (alien_color1 == "green") {
-    // Print a message that the player just earned 5 points
-    console.log("You just earned 5 points!");
-}
-// Output: (nothing)
+// Use the function to make three dictionaries representing different // albums 
+let album1 = make_album("Taylor Swift", "Red");
+let album2 = make_album("Ed Sheeran", "Divide", 16);
+let album3 = make_album("Adele", "25");
+// Print each return value to show that Objects are storing the // album information correctly 
+console.log(album1);
+console.log(album2);
+console.log(album3);
---
+++ exercise38/app.ts
@@ -0,0 +1,17 @@
+// Write a function called make_album() that builds a Object // describing a music album 
+function make_album(artist: string, title: string, tracks?: number) { // The function should take in an artist name and an // album title, and it should return a Object containing these two pieces of // information 
+  let album: {artist: string, title: string, tracks?: number} = { artist: artist, title: title };
+
+// Add an optional parameter to make_album() that allows you to store the // number of tracks on an album. If the calling line includes a value for the number // of tracks, add that value to the album’s Object 
+if (tracks) { album.tracks = tracks; }
+return album; }
+
+// Use the function to make three dictionaries representing different // albums 
+let album1 = make_album("Taylor Swift", "Red"); 
+let album2 = make_album("Ed Sheeran", "Divide", 16); 
+let album3 = make_album("Adele", "25");
+
+// Print each return value to show that Objects are storing the // album information correctly 
+console.log(album1); 
+console.log(album2); 
+console.log(album3);
