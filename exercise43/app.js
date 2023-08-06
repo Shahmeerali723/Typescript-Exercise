7188f0db839705b0581b5378e15243d04fbd072a
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-06 14:33:30 -0700

    Add Commit

--- exercise43/app.js
+++ exercise43/app.js
@@ -1,17 +1,16 @@
 "use strict";
-// Define a variable to store the alien's color
-let alien_color = "green";
-// Write an if statement to test whether the alien's color is green
-if (alien_color == "green") {
-    // Print a message that the player just earned 5 points
-    console.log("You just earned 5 points!");
+// Write a function that stores information about a car in a Object 
+function make_car(manufacturer, model, ...options) {
+    let car = { manufacturer: manufacturer, model: model };
+    // Use a for loop to iterate over the options array 
+    for (let option of options) { // Use array destructuring to get the key and value of each option 
+        let [key, value] = option; // Add the key and value to the car Object 
+        car[key] = value;
+    }
+    // Return the car Object 
+    return car;
 }
-// Output: You just earned 5 points!
-// Define a variable to store the alien's color
-let alien_color1 = "red";
-// Write an if statement to test whether the alien's color is green
-if (alien_color1 == "green") {
-    // Print a message that the player just earned 5 points
-    console.log("You just earned 5 points!");
-}
-// Output: (nothing)
+// Call the function with the required information and two other name-value pairs, such as a // color or an optional feature 
+let my_car = make_car("Tesla", "Model 3", ["color", "red"], ["autopilot", true]);
+// Print the Object that’s returned to make sure all the information was // stored correctly 
+console.log(my_car);
--- exercise43/app.ts
+++ exercise43/app.ts
@@ -1,21 +1,18 @@
-// Define a variable to store the alien's color
-let alien_color: string = "green";
+// Write a function that stores information about a car in a Object 
+function make_car(manufacturer: string, model: string, ...options: [string, any][]) { // The function should always receive a manufacturer and a model name // It should then accept an arbitrary number of keyword arguments // Create a Object to store the car information 
+  let car: {[key: string]: any} = { manufacturer: manufacturer, model: model };
 
-// Write an if statement to test whether the alien's color is green
-if (alien_color == "green") {
-  // Print a message that the player just earned 5 points
-  console.log("You just earned 5 points!");
+// Use a for loop to iterate over the options array 
+for (let option of options) { // Use array destructuring to get the key and value of each option 
+  let [key, value] = option; // Add the key and value to the car Object 
+  car[key] = value; }
+
+// Return the car Object 
+return car; 
 }
 
-// Output: You just earned 5 points!
+// Call the function with the required information and two other name-value pairs, such as a // color or an optional feature 
+let my_car = make_car("Tesla", "Model 3", ["color", "red"], ["autopilot", true]);
 
-// Define a variable to store the alien's color
-let alien_color1: string = "red";
-
-// Write an if statement to test whether the alien's color is green
-if (alien_color1 == "green") {
-  // Print a message that the player just earned 5 points
-  console.log("You just earned 5 points!");
-}
-
-// Output: (nothing)
+// Print the Object that’s returned to make sure all the information was // stored correctly 
+console.log(my_car);
