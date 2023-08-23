61e595f4d3f4321b3b56d99df952981a5c7e89af
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-23 15:25:01 -0700

    commit

--- exercise19/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let place = {
-    country: "pakistan",
-    city: "Karachi",
-    block: 15
-};
-console.log(place);
-console.log(place.country); // access the country same as its other
-console.log(place["block"]); // acces the block number
