ee2d9cc07f4c566cd164e37d44bf8787807c39f8
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-23 15:15:52 -0700

    commit

--- exercise04/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let famousPerson = "Albert Einstein";
-let message = famousPerson + "once said," + "“A person who never made a mistake never tried anything new.”";
-console.log(message);
