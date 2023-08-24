7d3dd8d04fa02ed8f41b89a1a3365fbbe6433395
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-24 16:27:47 -0700

    commit

--- exercise37/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Write a function called city_country() that takes in the name // of a city and its country 
-function city_country(city, country) {
-    return city + ", " + country;
-}
-// Call your function with at least three city-country pairs, and print the value // that’s returned 
-console.log(city_country("Karachi", "Pakistan"));
-console.log(city_country("Tokyo", "Japan"));
-console.log(city_country("New York", "USA"));
