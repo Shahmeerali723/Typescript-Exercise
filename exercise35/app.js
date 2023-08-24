2eb6d6f47d81675595b7006caf0c76b62b8496d3
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-24 16:27:00 -0700

    commit

--- exercise35/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function make_shirt(size = "large", message = "I love TypeScript") {
-    console.log("You ordered a " + size + " shirt with the message: " + message);
-}
-// Make a large shirt and a // medium shirt with the default message make_shirt(); 
-make_shirt("medium");
-// Make a shirt of any size with a different // message 
-make_shirt("small", "Typescript is awesome!");
