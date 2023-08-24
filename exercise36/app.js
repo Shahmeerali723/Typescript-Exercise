f3a364a95552bc773ec884088b2e4688137810da
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-24 16:27:23 -0700

    commit

--- exercise36/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Write a function called describe_city() that accepts the name of // a city and its country 
-function describe_city(city, country = "Pakistan") {
-    console.log(city + " is in " + country + ".");
-}
-// Call your function for three different cities, at least one of which is not in the // default country 
-describe_city("Karachi");
-describe_city("Islamabad");
-describe_city("Paris", "France");
