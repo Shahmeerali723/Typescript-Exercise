23965f4a3f73506e1379b894b7adbc7f81c39613
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-24 16:25:51 -0700

    commit

--- exercise32/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Think of at least three kinds of your favorite pizza. Store these // pizza names in a array 
-let pizzas = ["pepperoni", "cheese", "mushroom"];
-// Use a for loop to print the name of each pizza 
-for (let pizza of pizzas) { // Modify your for loop to print a sentence using the name of the pizza // instead of printing just the name of the pizza 
-    console.log("I like " + pizza + " pizza.");
-}
-// Add a line at the end of your program, outside the for loop, that states // how much you like pizza 
-console.log("I really love pizza!");
