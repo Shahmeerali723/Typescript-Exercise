f7cdfdbd17c8b503c509a40173bc56d35497a418
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-24 16:26:13 -0700

    commit

--- exercise33/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let animals = ["cat", "rabbit", "hamster"];
-// Use a for loop to print out the name of each animal 
-for (let animal of animals) { // Modify your program to print a statement about each animal, such as // A dog would make a great pet 
-    console.log("A " + animal + " would make a great pet.");
-}
-// Add a line at the end of your program stating what these animals have in // common. You could print a sentence such as Any of these animals would // make a great pet! 
-console.log("Any of these animals would make a great pet because they are cute, furry, and friendly!");
